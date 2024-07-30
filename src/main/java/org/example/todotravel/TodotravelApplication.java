4a40aabaaa31f2e4a2f1445abdc20bbfd1b8ce4b
PATRIC <wns0818@naver.com>
2024-07-30 10:26:08 +0900

    Feat: 기ë백엔드 기본 í베이스 작성

---
+++ src/main/java/org/example/todotravel/TodotravelApplication.java
@@ -0,0 +1,13 @@
+package org.example.todotravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodotravelApplication.class, args);
+    }
+
+}
