43574b7bbe9931256514f0b56ee5d6859b2c9df9
minjung <alswjd0240@naver.com>
2024-08-05 16:02:16 +0900

    Fix: 북마크, 좋아요 의존성 순환호출 오류 해결

--- src/main/java/org/example/todotravel/domain/plan/service/implement/LikeServiceImpl.java
+++ src/main/java/org/example/todotravel/domain/plan/service/implement/LikeServiceImpl.java
@@ -6,7 +6,6 @@ import org.example.todotravel.domain.plan.entity.Plan;
 import org.example.todotravel.domain.plan.repository.LikeRepository;
 import org.example.todotravel.domain.plan.service.LikeService;
 import org.example.todotravel.domain.user.entity.User;
-import org.example.todotravel.domain.user.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,14 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class LikeServiceImpl implements LikeService {
     private final LikeRepository likeRepository;
-    private final PlanServiceImpl planService;
-    private final UserServiceImpl userService;
 
     @Override
     @Transactional
-    public Like createLike(Long planId, Long userId) {
-        Plan plan = planService.getPlan(planId);
-        User user = userService.getUserByUserId(userId).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+    public Like createLike(Plan plan, User user) {
         Like like = Like.builder()
                 .likeUser(user)
                 .plan(plan)
@@ -31,9 +26,7 @@ public class LikeServiceImpl implements LikeService {
 
     @Override
     @Transactional
-    public void removeLike(Long planId, Long userId) {
-        Plan plan = planService.getPlan(planId);
-        User user = userService.getUserByUserId(userId).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+    public void removeLike(Plan plan, User user) {
         likeRepository.deleteByPlanAndLikeUser(plan, user);
     }
 
