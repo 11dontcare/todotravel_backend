c87064e529e2a50cb978dad6d77538ed4c0eac92
jini <rose7734@naver.com>
2024-08-12 14:22:46 +0900

    Feat: 알림 추가 및 Message 수정, vehicle 처리 완료

---
+++ src/main/java/org/example/todotravel/domain/chat/entity/ChatRoomDetail.java
@@ -0,0 +1,29 @@
+package org.example.todotravel.domain.chat.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_room_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id", nullable = false)
+    private Long roomId;
+
+    @Column(name = "room_name", nullable = false, length = 50)
+    private String roomName;
+
+    @Column(name = "room_date", nullable = false)
+    private LocalDateTime roomDate;
+}
