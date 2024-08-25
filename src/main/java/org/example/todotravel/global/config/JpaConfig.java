97728649f5e324dbed3867174c7bfd04de7016c9
honeyWater <tkd2756@naver.com>
2024-08-26 03:07:17 +0900

    Feat: JPA 트랜잭션 매니저 설정 추가

---
+++ src/main/java/org/example/todotravel/global/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package org.example.todotravel.global.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean(name = "transactionManager")
+    @Primary
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
