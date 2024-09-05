47b3a21230228e3518f35e1e56ffc414b2659e62
honeyWater <tkd2756@naver.com>
2024-09-05 14:19:31 +0900

    Feat: 비밀번호에 대한 유효성 주석 제거

--- src/main/java/org/example/todotravel/domain/user/dto/request/UserRegisterRequestDto.java
+++ src/main/java/org/example/todotravel/domain/user/dto/request/UserRegisterRequestDto.java
@@ -27,9 +27,9 @@ public class UserRegisterRequestDto {
     private String username;
 
     // 통합 테스트 시 추가 필요
-//    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
-//    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-//        message = "비밀번호는 대소문자, 숫자, 특수문자를 포함해야 합니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "비밀번호는 대소문자, 숫자, 특수문자를 포함해야 합니다.")
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
